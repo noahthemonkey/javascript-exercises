77533262d79ec4e55b3787d946331ee21921c318
Noah Msekni <msekninoah@gmail.com>
2022-03-30 22:42:12 +0200

    finally made it work but need to figure out why and how it worked

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,12 @@
-const removeFromArray = function(myArray, num) {
-    var myArray = [1, 2, 3, 4];
-    var values = [num] ;
-    myArray = myArray.filter(item => !values.indexOf(item) === -1);
-    
-    return myArray;
-    }
+const removeFromArray = function(myArray, ...theArgs) {   
 
+    const theArray = myArray.filter(removeThese); 
+
+    function removeThese(value){
+        return !theArgs.includes(value);
+    }
+    return theArray;
+}
 
 
 removeFromArray([1, 2, 3, 4], 3, 2);
