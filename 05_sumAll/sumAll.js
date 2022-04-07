77041c2a741b610ee24e649ee83a32f3352c5870
Noah Msekni <msekninoah@gmail.com>
2022-04-07 21:54:04 +0200

    solved 4 and 5, remove from array took me a long long long time, but starting to get the hang of javascript more and more i feel like.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function sumthese(start, end){
+    let result = 0;
 
-};
+    if (start > end) {
+        for (let i = end; i <= start; i++) {
+    
+            result += i;
+    
+    
+        }
+    }
+    else if (start < 0 || end < 0 ){
+        return "ERROR";
+}
+    else if (typeof start === "string"  || typeof end === "string" || typeof start === "object" || typeof end === "object") {
+        return "ERROR"
+    }
+    
+    else {
+        for (let i = start; i <= end; i++) {
+    
+            result += i;
 
+
+        }
+}
+    return result;
+}
 // Do not edit below this line
 module.exports = sumAll;
