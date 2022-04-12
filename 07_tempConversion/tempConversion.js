c042a46cafc2a9d40316b07e0b960902ee38924c
Noah Msekni <msekninoah@gmail.com>
2022-04-12 15:27:02 +0200

    finished, it was way easier than i thought and i finished it faster than expected too.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(f1) {
+    const c1 = (f1 - 32) *(5/9);
 
+    const c1Rounded = Math.round(c1 * 10) / 10;
+    return c1Rounded;
 };
 
-const ctof = function() {
+const ctof = function(c2) {
+    const f2 = (c2 *(9/5)) + 32;
 
+    const f2Rounded = Math.round(f2 * 10) / 10;
+    return f2Rounded;
+  
 };
 
 // Do not edit below this line
