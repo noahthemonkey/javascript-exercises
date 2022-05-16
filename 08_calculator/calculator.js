74e854958e71798b04963e88838011fd04de76fa
Noah Msekni <msekninoah@gmail.com>
2022-05-16 16:25:59 +0200

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(num1, num2) {
+  let addFunc = num1 + num2;
+  return addFunc
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  let subFunc = num1 - num2;
+  return subFunc;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let initNum = 0;
+  let sumFunc = arr.reduce((previousNum, currentNum) => previousNum + currentNum, initNum);
+  return sumFunc
+};
+
+const multiply = function(mul) {
+  let initMul = 1;
+  let sumMul = mul.reduce((mul1, mul2) => mul1 * mul2, initMul); 
+  return sumMul
+};
+
+const power = function(powerNum1, powerNum2) {
+  let powerFunc = powerNum1**powerNum2;
+  return powerFunc;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(factNum) {
+  let factArray = [];
+  for(i = 1; i <= factNum; i++ ) {
+    factArray.push(i)
+  }
+  let initFact = 1;
+	let factFunc = factArray.reduce((fact1, fact2) => fact1 * fact2, initFact)
+  return factFunc
 };
 
 // Do not edit below this line
