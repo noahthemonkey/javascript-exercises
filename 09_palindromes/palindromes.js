980dca3f7d7e338c6eff24496fdd0aa35e82133b
Noah Msekni <msekninoah@gmail.com>
2022-05-18 00:49:44 +0200

    complet 09, working on 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let re = /[\W_]/g;
+    let wordtoLow = word.toLowerCase().replace(re, '');
 
+    reverseWord = wordtoLow.split('').reverse().join('');
+    return reverseWord === wordtoLow
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function fibSum(nums) {
+    let fib = [0, 1];
+    let data = [];
+    
+    for(let i = 2; i <= nums; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2]; 
+      data.push(fib[i]);
+    }
+    
+    return data.pop();
 };
 
 // Do not edit below this line
