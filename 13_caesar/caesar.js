cb196e99ea48a1b722b39647088e2af4f3e7de0d
Noah Msekni <msekninoah@gmail.com>
2022-05-21 10:41:08 +0200

    DONE with 13 finally, felt difficult and dont really fully understand everything i did but i am happy that i solved it

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -7,18 +7,21 @@ const lower = 'abcdefghijklmnopqrstuvwxyz'
 const caesar = function(word, number) {
 
     let text = '';
+    var numMod = function numMod(n, m) {
+        return ((n % m) + m) % m;
+      }
 
     for(i = 0; i < word.length; i++){
     var letter = word[i];
 
     if(upper.includes(letter)) {
         // if it contains an uppercase charcter, add the number of shifts to it. If it exceeds MAX or MIN for UPPER then add or subtract from the beginning or the end.
-        let letterShift = ((upper.indexOf(letter) + number) % 26)
+        let letterShift = numMod((upper.indexOf(letter) + number), 26)
         text += upper[letterShift]
 
     }else if(lower.includes(letter)) {
          // if it contains an lowercase charcter, add the number of shifts to it. If it exceeds MAX or MIN for LOWER add or subtract from the beginning or the end.
-        let letterShift = ((lower.indexOf(letter) + number) % 26)
+        let letterShift = numMod((lower.indexOf(letter) + number), 26)
         text += lower[letterShift]
          
     }else {
